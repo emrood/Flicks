57f55ab7976e5d42183c5a218604e42849013fde
Noel Emmanuel Roodly <emmanuelroodly@yahoo.fr>
2017-07-20 13:40:33 -0400

    changement design

--- app/src/main/java/emrood/emmanuelroodly/flicksapp/MainActivity.java
+++ app/src/main/java/emrood/emmanuelroodly/flicksapp/MainActivity.java
@@ -1,26 +1,21 @@
 package emrood.emmanuelroodly.flicksapp;
 
-import android.net.Uri;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.TextHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 import cz.msebera.android.httpclient.Header;
 import emrood.emmanuelroodly.flicksapp.adapters.filmsAdapter;
@@ -31,11 +26,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Films> films;
     filmsAdapter myAdapter;
     ListView lvFilms;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,19 +38,21 @@ public class MainActivity extends AppCompatActivity {
         lvFilms.setAdapter(myAdapter);
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get(url, new JsonHttpResponseHandler() {
+        client.get(url, new JsonHttpResponseHandler(){
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 //super.onSuccess(statusCode, headers, response);
                 JSONArray filmJSONResults = null;
                 try {
-                    filmJSONResults = response.getJSONArray(Integer.parseInt("results"));
+                    filmJSONResults = response.getJSONArray("results");
                     myAdapter.notifyDataSetChanged();
                     films.addAll(Films.fromJSONArray(filmJSONResults));
                     Log.d("DEBUG", films.toString());
+                    Toast.makeText(MainActivity.this, "Connection", Toast.LENGTH_SHORT).show();
+
                 } catch (JSONException e) {
                     e.printStackTrace();
-
+                    Toast.makeText(MainActivity.this, "erreur", Toast.LENGTH_SHORT).show();
                 }
 
                 Log.d("debug", "connection succed");
@@ -69,65 +62,8 @@ public class MainActivity extends AppCompatActivity {
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 super.onFailure(statusCode, headers, responseString, throwable);
                 Log.d("debug", "connection failed");
+                Toast.makeText(MainActivity.this, "Connection echoue", Toast.LENGTH_SHORT).show();
             }
         });
-        /*
-        Log.d("debug", "1");
-
-        client.get("http://google.com", new TextHttpResponseHandler() {
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                Log.d("debug", "Connection fail");
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                Log.d("debug", "Succes");
-            }
-        });
-        */
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client2.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://emrood.emmanuelroodly.flicksapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client2, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://emrood.emmanuelroodly.flicksapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client2, viewAction);
-        client2.disconnect();
     }
 }
--- app/src/main/java/emrood/emmanuelroodly/flicksapp/adapters/filmsAdapter.java
+++ app/src/main/java/emrood/emmanuelroodly/flicksapp/adapters/filmsAdapter.java
@@ -1,6 +1,7 @@
 package emrood.emmanuelroodly.flicksapp.adapters;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +9,13 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import emrood.emmanuelroodly.flicksapp.R;
 import emrood.emmanuelroodly.flicksapp.models.Films;
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
 /**
  * Created by Emmanuel Roodly on 19/07/2017.
@@ -19,6 +23,11 @@ import emrood.emmanuelroodly.flicksapp.models.Films;
 public class filmsAdapter extends ArrayAdapter<Films>{
     public filmsAdapter(Context context, List<Films> films) {
         super(context, android.R.layout.simple_list_item_1, films);
+    }
+    Resources plh = getPlh();
+
+    public Resources getPlh() {
+        return plh;
     }
 
     @Override
@@ -37,7 +46,7 @@ public class filmsAdapter extends ArrayAdapter<Films>{
 
         tvTitle.setText(movie.getOriginalTitle());
         tvOverview.setText(movie.getOverview());
-
+        Picasso.with(getContext()).load(movie.getPosterPath()).transform(new RoundedCornersTransformation(30, 30)).placeholder(R.drawable.plh).into(ivFilms);
         return convertView;
     }
 }
